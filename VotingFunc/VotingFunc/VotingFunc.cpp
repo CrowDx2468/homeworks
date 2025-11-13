0b50d8589070e5b83897884d5662eb0a96e977bb
CrowDx2468 <leha.churkin.203@gmail.com>
2025-11-13 17:06:17 +0300

    upload

---
+++ VotingFunc/VotingFunc/VotingFunc.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+
+int yesCount = 0, noCount = 0;
+string answer;
+void voting(string);
+int main() {
+    
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    do {
+        cout << "Введите ваш голос (за / против / стоп): ";
+        cin >> answer;
+        if (answer != "стоп") {
+            voting(answer);
+            cout << "\nза:" << yesCount << "\nпротив:" << noCount << endl;
+        }
+    } while (answer != "стоп");
+
+    cout << "\nГолосование завершено!" << endl;
+    cout << "Итог:\nза: " << yesCount << "\nпротив: " << noCount << endl;
+
+}
+void voting(string answer) {
+    if (answer == "за") {
+        yesCount++;
+    }
+    else if (answer == "против") {
+        noCount++;
+    }
+}
